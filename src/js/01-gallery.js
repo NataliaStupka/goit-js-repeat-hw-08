cd87f4b7021cc0c7f1c942d0e1d37a733f52484a
NataliaStupka <NataliGluschenko@ukr.net>
2024-06-16 21:47:12 +0300

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items'; //масив зображень
 
-console.log(galleryItems);
+import { createMarkup } from '../helpers/01-gallery/createMarkup'; //функція розмітки
+
+import SimpleLightbox from 'simplelightbox'; //імпорт бібліотеки
+import 'simplelightbox/dist/simple-lightbox.min.css'; // Додатковий імпорт стилів бібліотеки
+
+// Change code below this line
+console.log('Home worck 8.1: "simplelightbox"');
+
+const list = document.querySelector('.js-gallery');
+
+//розмітка
+createMarkup(galleryItems, list);
+
+//модальне вікно
+new SimpleLightbox('.gallery a', {
+  captionPosition: 'bottom',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+console.log('Home worck 8.2: npm install @vimeo/player');
+//-------------------------------------------------------
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time'; //localStorage
+
+player.on('play', function (data) {
+  console.log('played the video!:', data);
+});
+
+// this!! updateTime
+player.on('timeupdate', throttle(updateTime, 1000));
+
+function updateTime(data) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds));
+}
+
+// function startVideo() {
+//   console.log('start');
+//   player.setCurrentTime().then(function () {
+
+//   });
+// }
+// startVideo();
+// player.setCurrentTime().then(function () {
+//   JSON.parse(localStorage.getItem(STORAGE_KEY));
+// });
+
+//----------------------
+// player.setVolume(0.4); //встановлення рівня звуку
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
