10bd05668bea40c16e392468d8ff482f51f3be9b
NataliaStupka <NataliGluschenko@ukr.net>
2024-06-17 12:34:28 +0300

    task 03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+const refs = {
+  form: document.querySelector('.js-feedback-form'),
+  email: document.querySelector('.js-feedback-form input'),
+  textarea: document.querySelector('.js-feedback-form textarea'),
+};
 
+const STORAGE_KEY = 'feedback-form-state'; //localStorage
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', onInputForm); //делегування на form, через target дістаємо до всіх полів
+
+const dataForm = {}; //обьект в який будемо зберігати дані з форми
+populateForm();
+function onInputForm(event) {
+  dataForm[event.target.name] = event.target.value; // в яке поле: що ввели
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm)); //зберігаємо у localStorage
+}
+//після відправки очистка форми і localStorage
+function onFormSubmit(event) {
+  event.preventDefault(); //заборона поведінки за замовчуванням, автоматичної перезавантаження сторінки
+  event.currentTarget.reset(); //очищаємо форму після відправки, reset очищає усі поля форми
+  localStorage.removeItem(STORAGE_KEY); //після відправки, очищуємо сховище
+}
+
+//при завантаженні сторінки
+function populateForm(event) {
+  const savedDataForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedDataForm) {
+    refs.email.value = savedDataForm.email;
+    refs.textarea.value = savedDataForm.message;
+  }
+}
